89e0b66007c6ad0e4900a4732a0e0c33909b03c2
Ankur Adarsh <77184810+ankuradarsh@users.noreply.github.com>
2023-08-20 00:10:36 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        int i=0,j=0,sum=0;
+        for(i=0;i<n;i++){
+            sum = sum + arr[i];
+            
+            while( j < i && sum > s ){
+                sum = sum - arr[j];
+                j++;
+            }
+            
+            if(sum == s){
+                break;
+            }
+        }
+        ArrayList<Integer> list = new ArrayList();
+        if(sum != s){
+            list.add(-1);
+            return list;
+        }
+        list.add(j+1);
+        list.add(i+1);
+        return list;
+    }
+}
